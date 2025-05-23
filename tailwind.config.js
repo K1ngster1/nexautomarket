fc741695bfc5d08d7ef21d867bca0a6a8a15a0ff
K1ngster1 <Vladyslav.planet@gmail.com>
2025-05-23 13:38:25 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // Додає підтримку темної теми через клас .dark
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}"
