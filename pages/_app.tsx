7838153b24527b972b8b91d203035843d59802a4
K1ngster1 <Vladyslav.planet@gmail.com>
2025-05-24 13:59:10 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
+// pages/_app.tsx
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import BottomNav from '../components/BottomNav'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <BottomNav />
+    </>
+  )
 }
