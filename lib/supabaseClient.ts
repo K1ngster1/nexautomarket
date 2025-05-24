afa10929432c5a11cfc9ea335b6837a2a6709982
K1ngster1 <Vladyslav.planet@gmail.com>
2025-05-24 21:23:48 +0300

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,1 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://...your-project-ref.supabase.co' // твій URL з Supabase
+const supabaseAnonKey = 'public-anon-key' // твій public key з Supabase
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
