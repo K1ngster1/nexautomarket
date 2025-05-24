1cc9e0b8a6626de40c836236486a6b146e5c4ca0
K1ngster1 <Vladyslav.planet@gmail.com>
2025-05-24 21:30:15 +0300

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://...your-project-ref.supabase.co' // твій URL з Supabase
-const supabaseAnonKey = 'public-anon-key' // твій public key з Supabase
+const supabaseUrl = 'https://kizoynsruttrsssuaina.supabase.co' // твій URL з Supabase
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtpem95bnNydXR0cnNzc3VhaW5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4NDcxMDIsImV4cCI6MjA2MzQyMzEwMn0.l3Dy2a9Uk9WngEPXOf4ize6GiSxaoWjw6y9VRQ80FoE' // твій public key з Supabase
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
