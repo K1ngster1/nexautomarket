3ed35fc16135efa16fa57ad0d4655655f9d7c5d3
K1ngster1 <Vladyslav.planet@gmail.com>
2025-05-24 21:34:16 +0300

    Create Car.ts

---
+++ types/Car.ts
@@ -0,0 +1,15 @@
+export type Car = {
+  id: string;
+  title: string;
+  price: number;
+  year: number;
+  mileage: number;
+  engine: string;
+  transmission: string;
+  drive: string;
+  vin: string;
+  status: string;
+  description: string;
+  created_at: string;
+  images: string;
+}
